1f36284f0701cada4e308345e09e07f37da98f29
Bruna Rafaela <Bruna@MacBook-Pro-de-Bruna.local>
2020-10-23 19:33:38 -0300

    Components added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import "./styles/pages/landing.css"; //import do css da landing
+import "leaflet/dist/leaflet.css";
+
 
 import Routes from './routes';
 
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { render } from "react-dom";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
 
 import mapMarketImg from '../images/map-marker.svg'; 
 
 import '../styles/pages/orphanages-maps.css';
+import mapIcon from '../utils/mapIcon';
 
 function OrphanagesMap(){
     return (
@@ -36,9 +36,23 @@ function OrphanagesMap(){
             url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
           />
            */}
+
+          <Marker icon={mapIcon} position={[-23.583688, -46.6746253]}>
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              Lar das meninas
+              <Link to="/orphanages/1">
+                <FiArrowRight size={20 } color="#FFF" />
+              </Link>
+            </Popup>
+          </Marker>
         </Map>
 
-        <Link to="" className="create-orphanage">
+        <Link to="orphanages/create" className="create-orphanage">
           <FiPlus size={32} color="#fff" />
         </Link>
       </div>
